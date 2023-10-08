e42a751809823833eb17e8622342f0a4133e19e7
Aspasija <aspasija.cvetkoska@result.si>
2023-10-08 23:34:36 +0200

    navbar for MERN Real Estate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import { About } from './pages/About';
+import { Booking } from './pages/Booking';
+import { Contact } from './pages/Contact';
+import { Home } from './pages/Home';
+import { Listings } from './pages/Listings';
+import { SignIn } from './pages/SignIn';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/listing' element={<Listings/>}></Route>
+        <Route path='/booking' element={<Booking/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/sign-in' element={<SignIn/>}></Route>
+      </Routes>
     </div>
   );
 }
